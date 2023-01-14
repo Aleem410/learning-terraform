316d0bbacbc18985f3fff403255da84693fa6063
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-14 19:49:25 +0400

    tera

--- 01-outputs/output.tf
+++ 01-outputs/output.tf
@@ -1,9 +1,8 @@
-
 data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners            = ["827190588206"]
+  owners           = ["827190588206"]
 }
 output "AMI_ID" {
-  value = "data.aws_ami.example.id"
+  value = data.aws_ami.example.id
   }
