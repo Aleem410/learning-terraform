e993be8eba9341edc645dfecde59c3b1e357b1aa
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-14 20:22:55 +0400

    tera

--- 01-outputs/output.tf
+++ 01-outputs/output.tf
@@ -7,7 +7,15 @@ data "aws_ami" "example" {
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
-#
-#output "AMI_ID" {
-#  value = data.aws_ami.example.id
-#}
+
+output "AMI_ID" {
+  value = data.aws_ami.example.id
+}
+
+data "aws_ec2_spot_price" "NEW" {
+  instance_type     = "t3.small"
+  availability_zone = "us-east-1"
+}
+output "PRICE" {
+  value = data.aws_ec2_spot_price.NEW.id
+}
