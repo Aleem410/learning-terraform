47e179cc5b0f22739b8eccf12095c3cbba649877
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-14 20:28:28 +0400

    tera

--- 01-outputs/output.tf
+++ 01-outputs/output.tf
@@ -15,6 +15,11 @@ output "AMI_ID" {
 data "aws_ec2_spot_price" "new" {
   instance_type     = "t3.small"
   availability_zone = "us-east-1a"
+
+  filter {
+    name   = "ami_name"
+    values = ["ami-0a017d8ceb274537d"]
+  }
 }
 output "PRICE" {
   value = data.aws_ec2_spot_price.new.id
