761e2a963cc26c01392e0c53fcebad9ef72fdfaa
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-15 21:48:37 +0400

    tera

--- 05-modules/ec2/ec2.tf
+++ 05-modules/ec2/ec2.tf
@@ -1,9 +1,13 @@
 resource "aws_spot_instance_request" "test" {
   ami                    = var.ami
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["var.sgid, var.allow_all"]
+  vpc_security_group_ids = [
+         var.sgid,
+         var.allow_all
+  ]
 }
 
 variable "sgid" {}
 variable "ami" {}
 variable "allow_all" {}
+
