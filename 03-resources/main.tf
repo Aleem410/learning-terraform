abfddd7e8c8278c12e2423a105e1026cf85a839f
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-15 12:59:32 +0400

    tera

--- 03-resources/main.tf
+++ 03-resources/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t3.small"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 }
 
 output "state" {
