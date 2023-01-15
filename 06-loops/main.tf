b801688a0202868e65093a03e9a020abe7c57808
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-15 22:09:17 +0400

    tera

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -3,3 +3,7 @@ resource "aws_spot_instance_request" "test" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
 }
+
+output "public_ip" {
+  value = aws_spot_instance_request.test.*.public_ip
+}
