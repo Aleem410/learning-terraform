94ad5308273505c78815d02dacb73459a9042fcc
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-15 13:31:08 +0400

    tera

--- 03-resources/main.tf
+++ 03-resources/main.tf
@@ -1,21 +1,39 @@
-#resource "aws_spot_instance_request" "test" {
-#  ami           = "ami-0a017d8ceb274537d"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "test"
-#  }
-#}
-#
-#output "public_ip" {
-#  value = aws_spot_instance_request.test.public_ip
-#
-#}
+resource "aws_spot_instance_request" "test" {
+  ami           = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
-resource "github_repository" "test_repository" {
-  name        = "test"
-  description = "My awesome codebase"
-  homepage_url = "https://github.com/Aleem410"
-  visibility = "public"
+  tags = {
+    Name = "test"
+  }
+}
 
+output "public_ip" {
+  value = aws_spot_instance_request.test.public_ip
+
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = vpc-00a494e89f26a41c5
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
