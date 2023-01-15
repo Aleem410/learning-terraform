cf85aa2cac813b2a5eef29af3a0b7c364b2bea0b
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-15 21:25:42 +0400

    tera

--- 05-modules/ec2/ec2.tf
+++ 05-modules/ec2/ec2.tf
@@ -1,9 +1,9 @@
-resource "aws_spot_instance_request" "test" {
-  ami                    = var.ami
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["var.sgid", "var.allow_all"]
-}
-
-variable "sgid" {}
-variable "ami" {}
-variable "allow_all" {}
+#resource "aws_spot_instance_request" "test" {
+#  ami                    = var.ami
+#  instance_type          = "t3.micro"
+#  vpc_security_group_ids = ["var.sgid", "var.allow_all"]
+#}
+#
+#variable "sgid" {}
+#variable "ami" {}
+#variable "allow_all" {}
--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -1,16 +1,16 @@
 module "sg" {
   source = "./sg"
 }
-
-module "ec2" {
-  source = "./ec2"
-  sgid = module.sg.sgid
-  ami = "ami-0a017d8ceb274537d"
-}
-
-module "ec21" {
-  source = "./ec2"
-  allow_all = module.sg.allow_all
-  ami = "ami-0b5eea76982371e91"
-}
+#
+#module "ec2" {
+#  source = "./ec2"
+#  sgid = module.sg.sgid
+#  ami = "ami-0a017d8ceb274537d"
+#}
+#
+#module "ec21" {
+#  source = "./ec2"
+#  allow_all = module.sg.allow_all
+#  ami = "ami-0b5eea76982371e91"
+#}
 
