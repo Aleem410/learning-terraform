848ff6e92e843fe4a83431de8ade8fb382a0b6ee
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-15 13:18:16 +0400

    tera

--- 03-resources/main.tf
+++ 03-resources/main.tf
@@ -1,13 +1,21 @@
-resource "aws_spot_instance_request" "test" {
-  ami           = "ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
+#resource "aws_spot_instance_request" "test" {
+#  ami           = "ami-0a017d8ceb274537d"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "test"
+#  }
+#}
+#
+#output "public_ip" {
+#  value = aws_spot_instance_request.test.public_ip
+#
+#}
 
-  tags = {
-    Name = "test"
-  }
-}
+resource "github_repository" "test_repository" {
+  name        = "test"
+  description = "My awesome codebase"
 
-output "public_ip" {
-  value = aws_spot_instance_request.test.public_ip
+  visibility = "public"
 
 }
