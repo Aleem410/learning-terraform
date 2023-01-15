d35835c3cc7abf9cb67aa7684895254c2f97fc9b
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-15 21:51:38 +0400

    tera

--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -5,12 +5,14 @@ module "sg" {
 module "ec2" {
   source = "./ec2"
   sgid = module.sg.sgid
+  allow_all = module.sg.allow_all
   ami = "ami-0a017d8ceb274537d"
 }
 
 module "ec21" {
   source = "./ec2"
   allow_all = module.sg.allow_all
+  sgid = module.sg.sgid
   ami = "ami-0b5eea76982371e91"
 }
 
