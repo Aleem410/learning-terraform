73a092bed8c6dbbe33af56a214e6d5c63cfd31f1
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-15 19:08:21 +0400

    tera

--- 03-resources/main.tf
+++ 03-resources/main.tf
@@ -1,39 +1,45 @@
-resource "aws_spot_instance_request" "test" {
-  ami           = "ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#resource "aws_spot_instance_request" "test" {
+#  ami           = "ami-0a017d8ceb274537d"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#
+#  tags = {
+#    Name = "test"
+#  }
+#}
+#
+#output "public_ip" {
+#  value = aws_spot_instance_request.test.public_ip
+#
+#}
+#
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic"
+#  vpc_id      = "vpc-00a494e89f26a41c5"
+#
+#  ingress {
+#    description      = "TLS from VPC"
+#    from_port        = 22
+#    to_port          = 22
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
+#
+#  tags = {
+#    Name = "allow_tls"
+#  }
+#}
 
-  tags = {
-    Name = "test"
-  }
-}
+variable "Games" {}
 
-output "public_ip" {
-  value = aws_spot_instance_request.test.public_ip
-
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "vpc-00a494e89f26a41c5"
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
+output "games" {
+  value = var.Games
 }
