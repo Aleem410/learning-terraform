a5c46592895487cedc879e2a11f4c540ae306759
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-16 22:12:25 +0400

    tera

--- 05-modules/sg/sg.tf
+++ 05-modules/sg/sg.tf
@@ -1,31 +1,31 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "vpc-00a494e89f26a41c5"
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
-output "sgid" {
-  value = aws_security_group.allow_tls.id
-}
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic"
+#  vpc_id      = "vpc-00a494e89f26a41c5"
+#
+#  ingress {
+#    description      = "TLS from VPC"
+#    from_port        = 22
+##    to_port          = 22
+##    protocol         = "tcp"
+##    cidr_blocks      = ["0.0.0.0/0"]
+##  }
+##
+##  egress {
+##    from_port        = 0
+##    to_port          = 0
+##    protocol         = "-1"
+##    cidr_blocks      = ["0.0.0.0/0"]
+##  }
+##
+##  tags = {
+##    Name = "allow_tls"
+##  }
+##}
+##
+##output "sgid" {
+##  value = aws_security_group.allow_tls.id
+##}
 
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
