376d538e99e120dc4fceb96b1e0c795efd3e8e2c
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-16 22:28:26 +0400

    tera

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -3,25 +3,14 @@ resource "aws_spot_instance_request" "test1" {
   #  ami           = var.amis[count.index]
   ami           = element(var.amis, count.index )
   instance_type = "t3.micro"
-  vpc_security_group_ids = var.sgid
   tags = {
     Name = "ami-${count.index}"
   }
 }
 
-variable "sgid" {
-  default = "sg-0d0b44e1148e9b818"
-}
-
-output "sgid" {
-  value = "var.sgid"
-}
 variable "amis" {
   default = [
     "ami-0a017d8ceb274537d",
     "ami-0b5eea76982371e91"
   ]
-}
-output "amis" {
-  value = "var.amis"
 }
