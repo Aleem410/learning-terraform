d7c6cd76af86483dd33fe571f22551be6b7c92fe
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-16 20:09:45 +0400

    tera

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -1,12 +1,29 @@
-resource "aws_spot_instance_request" "test" {
-  count         = 2
-  ami           = "ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
-  tags = {
-    Name = "instance-${count.index}"
-  }
+#resource "aws_spot_instance_request" "test" {
+#  count         = 2
+#  ami           = "ami-0a017d8ceb274537d"
+#  instance_type = "t3.micro"
+#  tags = {
+#    Name = "instance-${count.index}"
+#  }
+#}
+#
+#output "public_ip" {
+#  value = aws_spot_instance_request.test.*.public_ip
+#}
+
+variable "amis" {
+  default = [
+    "ami-0a017d8ceb274537d",
+    "ami-0b5eea76982371e91"
+
+]
 }
 
-output "public_ip" {
-  value = aws_spot_instance_request.test.*.public_ip
+resource "aws_spot_instance_request" "test" {
+  count         = 2
+  ami           = "var.amis[count.index]"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "ami-${count.index}"
+  }
 }
