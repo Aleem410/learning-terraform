a287b46675f22ac78508736154700204a4ac9c94
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-16 21:47:04 +0400

    tera

--- 06-loops/vars/vars.tf
+++ 06-loops/vars/vars.tf
@@ -13,7 +13,7 @@ resource "aws_spot_instance_request" "test1" {
   #  ami           = var.amis[count.index]
   ami           = element(var.amis, count.index )
   instance_type = "t3.micro"
-  vpc_security_group_ids = var.sgid
+  vpc_security_group_ids = "var.sgid"
   tags = {
     Name = "ami-${count.index}"
   }
