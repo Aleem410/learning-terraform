42640e418ba67226c9a0f23c251e877da54bddfc
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-16 20:16:11 +0400

    tera

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -19,7 +19,7 @@ variable "amis" {
 }
 resource "aws_spot_instance_request" "test1" {
   count         = 2
-  ami           = "var.amis[count.index]"
+  ami           = var.amis[count.index]
   instance_type = "t3.micro"
   tags = {
     Name = "ami-${count.index}"
