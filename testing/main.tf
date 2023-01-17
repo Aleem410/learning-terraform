24d5590a6833907bef608226c1ea1afccd8f0229
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-17 13:29:32 +0400

    tera

--- testing/main.tf
+++ testing/main.tf
@@ -8,8 +8,8 @@ module "ec" {
   ami = "ami-0a017d8ceb274537d"
 }
 
-module "ec1" {
-  source = "./ec"
-  sgid = module.security_group.sgid
-  ami = "ami-0b5eea76982371e91"
-}
+#module "ec1" {
+#  source = "./ec"
+#  sgid = module.security_group.sgid
+#  ami = "ami-0b5eea76982371e91"
+#}
