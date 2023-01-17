97ede4d89c31e05a449b7f3c5760cfa68d0f2f73
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-17 13:08:13 +0400

    tera

--- testing/ec/ec.tf
+++ testing/ec/ec.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "test" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
-  vpc_security_group_ids = var.sgid
+  vpc_security_group_ids = "var.sgid"
 
   tags = {
     Name = "test"
