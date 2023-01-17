72368ced11d97373cf2185c7a0b5b1b75ec53acd
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-17 13:37:15 +0400

    tera

--- testing/ec/ec.tf
+++ testing/ec/ec.tf
@@ -1,6 +1,6 @@
 resource "aws_spot_instance_request" "test" {
   count         = 2
-  ami           = var.ami
+  ami           = var.ami[count.index]
   instance_type = "t3.micro"
   vpc_security_group_ids = [var.sgid]
 
--- testing/ec/variable.tf
+++ testing/ec/variable.tf
@@ -1,2 +1,7 @@
 variable "sgid" {}
-variable "ami" {}
+variable "ami" {
+  default = [
+    "ami-0a017d8ceb274537d",
+    "ami-0b5eea76982371e91"
+  ]
+}
