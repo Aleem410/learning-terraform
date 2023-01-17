f7492439b3cf216d1d0536a02de71e26d5c42a58
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-17 10:59:16 +0400

    tera

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,1 +1,9 @@
+resource "aws_spot_instance_request" "test" {
+  ami           = "ami-0a017d8ceb274537d"
+  count  = 2
+  instance_type = "t3.micro"
 
+  tags = {
+    Name = "instance-$(count.index)"
+  }
+}
