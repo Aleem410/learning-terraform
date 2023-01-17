cc650b515b7a726269937cfcf0ca8946ab85577e
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-17 10:55:03 +0400

    tera

--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -6,13 +6,12 @@ module "ec2" {
   source = "./ec2"
   sgid = module.sg.sgid
   allow_all = module.sg.allow_all
-  ami = "ami-0a017d8ceb274537d"
+  ami  = "ami-0a017d8ceb274537d"
 }
 
 module "ec21" {
   source = "./ec2"
-  allow_all = module.sg.allow_all
   sgid = module.sg.sgid
-  ami = "ami-0b5eea76982371e91"
+  allow_all = module.sg.allow_all
+  ami  = "ami-0b5eea76982371e91"
 }
-
--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,16 +1,1 @@
-resource "aws_spot_instance_request" "test1" {
-  count         = 2
-  #  ami           = var.amis[count.index]
-  ami           = element(var.amis, count.index )
-  instance_type = "t3.micro"
-  tags = {
-    Name = "ami-${count.index}"
-  }
-}
 
-variable "amis" {
-  default = [
-    "ami-0a017d8ceb274537d",
-    "ami-0b5eea76982371e91"
-  ]
-}
