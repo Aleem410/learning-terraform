91d1240eee7d21b28206628fac4444c0aab6c07f
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-21 21:29:55 +0400

    robo

--- 02-variables/variable.tf
+++ 02-variables/variable.tf
@@ -5,47 +5,47 @@ output "sample" {
   value = var.sample
 }
 
-variable "testing" {
-  default = [
-   "hello",
-    100
-  ]
-}
-output "result" {
-  value = var.testing[0]
-}
-
-#accessing variables with functions (element)
-output "value" {
-  value = element(var.testing, 1)
-}
-
-variable "sampleXX" {
-  default = {
-    string = "hello"
-    number = 100
-  }
-}
-
-output resultXX {
-  value = var.sampleXX["number"]
-}
-
-#accessing variables map with lookup
-output resultYY {
-  value = lookup(var.sampleXX, "string", null)
-
-}
-
-variable "fruits" {}
-
-output FRUITS {
-  value = var.fruits
-}
-
-#variable "vegetables" {}
-#
-#output "veg" {
-#  value = var.vegetables
+#variable "testing" {
+#  default = [
+#   "hello",
+#    100
+#  ]
 #}
-
+#output "result" {
+#  value = var.testing[0]
+#}
+#
+##accessing variables with functions (element)
+#output "value" {
+#  value = element(var.testing, 1)
+#}
+#
+#variable "sampleXX" {
+#  default = {
+#    string = "hello"
+#    number = 100
+#  }
+#}
+#
+#output resultXX {
+#  value = var.sampleXX["number"]
+#}
+#
+##accessing variables map with lookup
+#output resultYY {
+#  value = lookup(var.sampleXX, "string", null)
+#
+#}
+#
+#variable "fruits" {}
+#
+#output FRUITS {
+#  value = var.fruits
+#}
+#
+##variable "vegetables" {}
+##
+##output "veg" {
+##  value = var.vegetables
+##}
+#
